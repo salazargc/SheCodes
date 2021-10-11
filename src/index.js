d04c0d51074513296191d5f73c8d325b44cc5acf
salazargc <74729190+salazargc@users.noreply.github.com>
2021-10-11 11:54:45 -0600

    Added forecast function to JS

--- src/index.js
+++ src/index.js
@@ -22,6 +22,34 @@ function formatDate(timestamp) {
   let day = days[weekDays];
   return `Last updated: ${day} ${hour}:${minutes}`;
 }
+// Forecast
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+    <div class="col-2">
+      <div class="weather-forecast-date">${day}</div>
+      <img
+        src="https://openweathermap.org/img/wn/02n@2x.png"
+        alt=""
+        width="42"
+      />
+      <div class="weather-forecast-temperatures">
+        <span class="weather-forecast-temperature-max">18°</span>
+        <span class="weather-forecast-temperature-min">12°</span>
+      </div>
+    </div>`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+displayForecast();
 
 // Show City and weather conditions
 
