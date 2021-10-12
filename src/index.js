a280161b8949b18bc63a48c95d8d5a6e2b8b114c
salazargc <74729190+salazargc@users.noreply.github.com>
2021-10-11 19:20:45 -0600

    error fixed in line 40 and one line for apiUrl

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (forecastDay, index) {
+  forecast.forEach(function (forecastDay, index) {
     if (index < 6) {
       forecastHTML =
         forecastHTML +
@@ -86,9 +86,7 @@ function inputCity(city) {
 
 function getForecast(coordinates) {
   let apiKey = "610aded4732d92a616becc2092c41d04";
-  let units = "metric";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?
-  lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=${units}`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
